178b5d162275c58d5cdc410f174b36b47b350f47
Tapan <108143145+tapansnowflake@users.noreply.github.com>
2022-06-28 18:10:29 +0530

    Update streamlit_app.py
    replaced title with text for body

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
 
-streamlit.title("Omega 3 & Blueberry Oatmeal")
-streamlit.title("Kale, Spinach & Rocket Smoothie")
-streamlit.title("Hard-Boiled Free-Range Egg")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
